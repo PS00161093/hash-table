368af1fdb6a3fc32d99dc31e11994d5647762535
ps29653 <SharmaPrashant@JohnDeere.com>
2020-11-25 20:27:30 +0530

    Full code

--- src/FirstRecurring.java
+++ src/FirstRecurring.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Hashtable;
 
 public class FirstRecurring {
 
@@ -6,6 +6,21 @@ public class FirstRecurring {
 
         int[] array = {3, 4, 5, 2, 3, 4};
 
-        Hashtable hashtable = new Hashtable();
+        System.out.println(getFirstRecurringItem(array));
+    }
+
+    private static int getFirstRecurringItem(int[] array) {
+
+        int duplicate = 0;
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+
+        for (int item : array) {
+            if (hashtable.containsKey(item)) {
+                duplicate = item;
+            } else {
+                hashtable.put(item, 1);
+            }
+        }
+        return duplicate;
     }
 }
