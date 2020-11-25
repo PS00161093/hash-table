136ae8e892766f0b955e19f7e27177a0c2256fb5
ps29653 <SharmaPrashant@JohnDeere.com>
2020-11-25 20:15:55 +0530

    FirstRecurring ex

---
+++ src/FirstRecurring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class FirstRecurring {
+
+    public static void main(String[] args) {
+
+        int[] array = {3, 4, 5, 2, 3, 4};
+
+        Hashtable hashtable = new Hashtable();
+
+    }
+}
